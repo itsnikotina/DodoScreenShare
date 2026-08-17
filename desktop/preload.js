06cf43fb5948c430b6dc202a143dbb363492fcac
Nikotina <nikotina@users.noreply.github.com>
2026-08-17 18:36:24 -0300

    fix: restore startNativeStereoAudio in preload.cjs and preload.js

--- desktop/preload.js
+++ desktop/preload.js
@@ -2,5 +2,15 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   getDesktopSources: () => ipcRenderer.invoke('get-desktop-sources'),
-  getAppInfo: () => ipcRenderer.invoke('get-app-info')
+  setActiveCaptureSource: (sourceId) => ipcRenderer.invoke('set-active-capture-source', sourceId),
+  ensureAudioIsolation: () => ipcRenderer.invoke('ensure-audio-isolation'),
+  checkAppUpdate: () => ipcRenderer.invoke('check-app-update'),
+  reloadApp: () => ipcRenderer.invoke('reload-app'),
+  onAppUpdated: (callback) => ipcRenderer.on('app-updated', (_event, value) => callback(value)),
+  getAppInfo: () => ipcRenderer.invoke('get-app-info'),
+
+  // Captura e streaming nativo de áudio estéreo (parec / pw-record 48kHz L/R)
+  startNativeStereoAudio: () => ipcRenderer.invoke('start-native-stereo-audio'),
+  stopNativeStereoAudio: () => ipcRenderer.invoke('stop-native-stereo-audio'),
+  onNativeAudioChunk: (callback) => ipcRenderer.on('native-audio-chunk', (_event, value) => callback(value))
 });
