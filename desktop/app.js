c9ebd647bf0e1f70f3d4c921824534faaff5d142
Nikotina <nikotina@users.noreply.github.com>
2026-08-17 18:03:15 -0300

    fix(host): enforce explicit play() on liveVideoPreview to guarantee continuous active frame capture

--- desktop/app.js
+++ desktop/app.js
@@ -542,6 +542,8 @@ async function startNativeScreenSharing(sourceId, resolution = '720p', fps = 30,
 
     // UI Updates
     dom.liveVideoPreview.srcObject = stream;
+    dom.liveVideoPreview.muted = true;
+    dom.liveVideoPreview.play().catch(() => {});
     dom.liveVideoPreview.classList.remove('hidden');
     dom.previewPlaceholder.classList.add('hidden');
     dom.liveStreamBadge.classList.remove('hidden');
